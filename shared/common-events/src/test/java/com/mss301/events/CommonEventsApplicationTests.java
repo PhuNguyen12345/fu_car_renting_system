1f3e2645cb1cbfc110236e76215748f31148717e
PhuNguyen12345 <phukak12345@gmail.com>
2026-07-02 12:17:01 +0700

    feat: initial commit

---
+++ shared/common-events/src/test/java/com/mss301/events/CommonEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss301.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
